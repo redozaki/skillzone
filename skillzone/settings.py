5ebad1a0c3cc2d82df5dd7286aacf3914a1440fe
redozaki <sofiaredo981@gmail.com>
2025-03-23 22:50:57 +0100

    home links to courses, users

--- skillzone/settings.py
+++ skillzone/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'skillzone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "skillzone"/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- skillzone/views.py
+++ skillzone/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import HttpResponse
-
+from django.http import HttpResponse
+from django.template import loader
+# Create your views here.
 def home(request):
-    return HttpResponse("You're at the skillzone index.")
+	template = loader.get_template('home.html')
+	return HttpResponse(template.render())
