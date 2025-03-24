09194ba1ec4952fa68ac226f3669e3c87d75bf10
redozaki <sofiaredo981@gmail.com>
2025-03-24 23:06:02 +0100

    added points field to User model

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    points = models.IntegerField(default=0)
 
     def __str__(self):
         return self.username
